5191bf96a2833704c8e6d3193f2ad5c1bf916d6c
365saiom <saiom365@naver.com>
2023-07-12 04:36:54 +0900

    add some

--- saintjames/section3.js
+++ saintjames/section3.js
@@ -1,13 +1,20 @@
-const container3 = document.querySelector(".container3");
-const box2 = document.createElement("div");
-box2.classList.add("box2");
+const main = document.querySelector("main");
+const section3 = main.querySelector(".section3");
+
+const article2 = document.createElement("article");
+article2.classList.add("article2");
+
+const item = document.createElement("div");
+item.classList.add("item");
+
+
 const juices = [
     "PASSIONFRUIT & PEACH TEA",
     "ORIGINAL GREEN TEA",
     "PINEAPPLE & MANGO TEA",
     "BLUEBERRY & RASPBERRY TEA",
     "BLOOD ORANGE & HIBISCUS TEA",
-];
+]
 
 const juiceTaste = [
     "Subtly sweet",
@@ -18,17 +25,20 @@ const juiceTaste = [
 ]
 
 for(let i=0; i<juices.length; i++){
+    
     const img = document.createElement("img");
     const h3 = document.createElement("h3");
     const div = document.createElement("div");
-    const article = document.createElement("article");
-    
+    const item = document.createElement("div");
+
+    item.classList.add("item")
+
     // img
     img.setAttribute("src", `./img/juice${i+1}.png`);
     img.setAttribute("alt", `juice${i+1} 사진`);
-    img.classList.add("juice-box")
     img.setAttribute("style","max-width:200px; max-height:300px; width:100%; height:100%;");
     img.setAttribute("width", "500px");
+    
     // h3
     h3.textContent = juices[i];
 
@@ -36,11 +46,8 @@ for(let i=0; i<juices.length; i++){
     div.innerText = juiceTaste[i];
 
     // article
-    article.append(img, h3, div);
-    box2.append(article);
-    container3.append(box2);
-}
+    item.append(img, h3, div);
+    article2.append(item);
+    section3.append(article2);
 
-window.addEventListener("scroll", () =>{
-    console.log(scrollY);
-})
+}
